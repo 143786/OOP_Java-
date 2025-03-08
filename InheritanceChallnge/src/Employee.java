0f636567d22f222c76ee3c645fb9b02340205a91
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2025-03-09 00:59:07 +0100

    Add files via upload

--- InheritanceChallnge/src/Employee.java
+++ InheritanceChallnge/src/Employee.java
@@ -2,11 +2,19 @@ public class Employee extends Worker {
 
     private long employeeId;
     private String hireDate;
+    private static int employeeNo = 1;
+    /*
+    Static field is a place that lets you share data among all
+    your instance. When we're generating an ID for a new employee,
+    it's a place to find the next ID to use.
+    */
 
-    public Employee(String name, String birthDate, long employeeId,
+
+
+    public Employee(String name, String birthDate,
                     String hireDate) {
         super(name, birthDate);
-        this.employeeId = employeeId;
+        this.employeeId = Employee.employeeNo++;
         this.hireDate = hireDate;
     }
 
--- InheritanceChallnge/src/Main.java
+++ InheritanceChallnge/src/Main.java
@@ -1,12 +1,27 @@
+import java.rmi.UnexpectedException;
+
 public class Main {
     public static void main(String[] args) {
 
         Employee tim = new Employee("Tim", "11/11/1985",
-                77001, "01/01/2020");
+                 "01/01/2020");
 
         System.out.println(tim);
         System.out.println("Age = " + tim.getAge());
         System.out.println("Pay = " + tim.collectPay());
+        // System.out.println("EmployeeID " + Employee.employeeID);
 
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/10/1990",
+                "02/03/2023", 35000);
+        System.out.println(joe);
+        System.out.println("Joe's paycheck = $" + joe.collectPay());
+        joe.retire();
+        System.out.println("Joe's pension check = $" + joe.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970",
+              "03/03/2021",  15 );
+        System.out.println(mary);
+        System.out.println("Mary's Paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday Pay = $" + mary.getDoublePay());
     }
 }
